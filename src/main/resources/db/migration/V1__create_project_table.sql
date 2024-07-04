e8ccf2945e09fbe664de96197441c9956b6c73bd
Thomas Sargent <thomas.sargent@kainos.com>
2024-07-04 10:39:18 +0100

    Added Projects table

--- src/main/resources/db/migration/V1__create_project_table.sql
+++ src/main/resources/db/migration/V1__create_project_table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE Project (
     project_Id smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    project_name varchar(30),
+    project_name varchar(50),
     project_value decimal(11, 2),
     client_name varchar(30)
 );
 
 INSERT INTO Project(project_name, project_value, client_name)
 VALUES
-("Games Unlimited games but no bacon", 2000.00, "Tiktok"),
+("Games Unlimited games", 2000.00, "Tiktok"),
 ("Hello Kitty Brawlhalla", 1000000.00, "CapCom"),
-("New Big Crypto Game", 60000.00, "Scammer INC UK");
+("New Big Crypto Game", 60000.00, "Company UK");
+
